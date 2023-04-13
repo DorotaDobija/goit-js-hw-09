18026b3ff6f9c6b62404eb347050bc1eb79b4ff0
DorotaDobija <dorota.dobija93@gmail.com>
2023-04-13 17:09:47 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
