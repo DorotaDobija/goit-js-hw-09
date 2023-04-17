4480f21d417d3fc36274b63efe3d9b50e2816011
DorotaDobija <dorota.dobija93@gmail.com>
2023-04-17 20:00:43 +0200

    task 03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,32 +11,32 @@ function promisFunc() {
   let delay = +firstDelay;
 
   for (let i = 1; i <= amount; i++) {
-    setTimeout(() => {
-      let position = i;
-      createPromise(position, delay)
-        .then(({ position, delay }) =>
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-        )
-        .catch(({ position, delay }) =>
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`)
-        );
-      delay = delay += +delayStep;
-    }, delayStep);
+    let position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) =>
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    delay = delay += +delayStep;
   }
 }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
 
 formEl.addEventListener('submit', e => {
   e.preventDefault();
-  setTimeout(() => promisFunc(), delayEl.value);
+  promisFunc();
 });
